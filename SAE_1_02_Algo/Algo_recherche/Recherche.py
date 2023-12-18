4bc111fbf78a305dd27bb46b93a97119676d2f05
Koenig Thomas <thomas.koenig@etu.univ-lyon1.fr>
2023-12-18 17:36:42 +0100

    algo recherche

--- SAE_1_02_Algo/Algo_recherche/Recherche.py
+++ SAE_1_02_Algo/Algo_recherche/Recherche.py
@@ -1,6 +1,6 @@
 import random
 
-
+#Création des tables
 tab1_10 = []
 tab2_10 = []
 tab3_10 = []
@@ -38,6 +38,7 @@ tab_10 = [tab1_10,tab2_10,tab3_10,tab4_10,tab5_10,tab6_10,tab7_10,tab8_10,tab9_1
 tab_500 = [tab1_500,tab2_500,tab3_500,tab4_500,tab5_500,tab6_500,tab7_500,tab8_500,tab9_500,tab10_500]
 tab_5000 = [tab1_5000,tab2_5000,tab3_5000,tab4_5000,tab5_5000,tab6_5000,tab7_5000,tab8_5000,tab9_5000,tab10_5000]
 
+#Insertion des valeurs dans les tables
 i=0
 while i < 10:
     j=0
@@ -67,8 +68,8 @@ while i < 5000:
 
 #Meilleur algo pour trier un tableau
 #tab 10 : 261.6
-#tab 500 : 383163.6
-#tab 5000 : 37589478.0
+#tab 500 : 383 163.6
+#tab 5000 : 37 589 478.0
 def tri_selection(tab):
     compteur = 0
     i = 0
@@ -90,7 +91,7 @@ def tri_selection(tab):
         tmp = tab[i]
         tab[i] = tab[min]
         tab[min] = tmp
-        i+=1
+        i = i + 1
         compteur =5+compteur
 
     compteur=1+compteur
@@ -98,15 +99,16 @@ def tri_selection(tab):
 
 
 #Algo pour rechercher un nom de compagnie ou la destination
+#Recherche par dichotomie
 #tab 10 : 21
-#tab 500 : 
-#tab 5000 :
+#tab 500 : 63.6
+#tab 5000 : 83.8
 def recherche_dicho(tab:[int],e:int):
     compteur=0
 
     deb=0
     fin=len(tab)-1
-    ind=-1
+    ind= -1
 
     compteur = 4 + compteur
 
@@ -129,41 +131,117 @@ def recherche_dicho(tab:[int],e:int):
 
             ind = millieu
             compteur = 3 + compteur
-            return ind,compteur
+            return compteur
         
     compteur = 1 + compteur
 
+    return compteur
+
+#Recherche dicho récursive
+#tab 10 :33.8
+#tab 500 : 79.0
+#tab 5000 : 100.2
+def recherche_dicho_rec(tab,e):
+    compteur = 0
+    n=len(tab)
+    millieu = n//2
+    ind = 0
+
+    compteur = compteur + 4
+
+    if n == 0:
+        ind = ind - 1
+
+        compteur = compteur + 3
+    else:
+        compteur = compteur + 1
+
+        if tab[millieu] == e:
+
+            ind = millieu
+            compteur = compteur + 2
+
+        elif tab[millieu] > e:
+
+            ind ,compteur_rec = recherche_dicho_rec(tab[:millieu],e)
+            compteur = compteur + 3 + compteur_rec
+
+        else:
+
+            ind, compteur_rec = recherche_dicho_rec(tab[millieu+1:],e)
+            compteur = compteur + 2 + compteur_rec
+
+            if ind != -1:
+
+                ind = ind + millieu + 1
+                compteur = compteur + 4
+
+            compteur = compteur + 1
     return ind,compteur
 
 
-#Tri par recherche pour trouver le nom ou la comagnie
-#tab 10 : 
-#tab 500 : 
-#tab 5000 : 
-def recherche_cour(tab:[int],e:int):
+
+#Tri par recherche pour trouver l'indice de la premiere ocurence
+#ou tri linéaire
+#tab 10 : 55.0
+#tab 500 : 571.0
+#tab 5000 : 1 182.4
+def recherche_prem_occ(tab:[int],e:int):
+    compteur = 0
     n=len(tab)
     ind=0
     i=0
+    compteur = compteur + 3
     while i < n and tab[ind] != e:
-        ind = ind + 1
-        i+=1
-    return ind
 
+        ind = ind + 1
+        i = i + 1
+        compteur = compteur + 6
+
+    compteur = compteur + 1
+    return compteur
+
+#Nombre d'occurence d'un term
+#tab 10 : 42.0
+#tab 500 : 2011.2
+#tab 5000 : 20 098.2
+def nbocc(tab:[int],e:int):
+    compteur = 0
+    i=0
+    nb=0
+    compteur = compteur + 2
+    while i < len(tab):
+        compteur = compteur + 1
+        if e==tab[i]:
+            nb = nb + 1
+            compteur = compteur + 2
+        i=i+1
+        compteur = compteur + 3
+    return compteur
 
 
 calcul = 0
 i=0
 
-
 while i < 10:
-    ind, compteur = recherche_dicho(tab_5000[i],50)
-    calcul = calcul + compteur
+    value = nbocc(tab_10[i],50)
+    calcul = calcul + value
     i = i+1
-print(f"La moyenne d'operation elementaire est de : {calcul/10}")
+print(f"La moyenne d'operation elementaire pour les tableaux a 10 est de : {calcul/10}")
 
-'''
+calcul = 0
+i = 0
 while i < 10:
-    calcul = calcul + tri_selection(tab_10[i],2)
+    value = nbocc(tab_500[i],50)
+    calcul = calcul + value
     i = i+1
-print(f"La moyenne d'operation elementaire est de : {calcul/10}")
-'''
+print(f"La moyenne d'operation elementaire pour les tableaux a 500 est de : {calcul/10}")
+
+calcul = 0
+i = 0
+while i < 10:
+    value = nbocc(tab_5000[i],50)
+    calcul = calcul + value
+    i = i+1
+    
+print(f"La moyenne d'operation elementaire pour les tableaux a 5000 est de : {calcul/10}")
